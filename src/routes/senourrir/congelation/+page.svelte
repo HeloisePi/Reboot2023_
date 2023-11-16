e8bcf125df70f18646a1e7a6511d0e9a5a476e8b
evemarielouis <eve.tissot@mmibordeaux.com>
2023-11-16 17:55:35 +0200

    color green

--- src/routes/senourrir/congelation/+page.svelte
+++ src/routes/senourrir/congelation/+page.svelte
@@ -6,15 +6,22 @@
 <HeroTheme srcImg="/images/logo/cold.svg" title="Règles de 
 congélation"></HeroTheme>
 <section>
-<Contenaire><p>T'as déjà pensé à la congélation comme super-pouvoir contre le gaspi? Ça vaut le coup de creuser cette idée. <br><br>
-    Des restes de pizza ou un surplus de lasagnes? Pas de panique, congèle-les! C'est pas juste pratique, c'est aussi un geste de guerre contre le gâchis alimentaire. C'est cool, économique, et ça donne une deuxième chance à tes repas.<br><br>
+<Contenaire><p>T'as déjà pensé <span class="green">à la congélation comme super-pouvoir contre le gaspi?</span> Ça vaut le coup de creuser 
+    cette idée. </p>
+    <p>Des restes de pizza ou un surplus de lasagnes? Pas de panique, 
+    congèle-les! C'est pas juste pratique, c'est aussi un geste de guerre 
+    contre le gâchis alimentaire. C'est cool, économique, et ça donne une 
+    deuxième chance à tes repas.</p>
     
     
-    Alors, la prochaine fois que t'as trop cuisiné ou que tu sens que tes fraises vont rendre l'âme, ouvre grand la porte du frigo, prépare tes sacs de congélation, et fais un dab pour dire adieu au gaspi.<br><br>
-    Mais attention cela ne se fait pas n’importe comment, pour être sûr que ta nourriture est safe !<br><br>
-    
-    
-    https://www.albal.fr/fr/les-10-regles-dor-pour-bien-congeler-et-decongeler-ses-aliments-612.html<br></p>
+    <p>Alors, la prochaine fois que t'as trop cuisiné ou que tu sens que tes 
+    fraises vont rendre l'âme, ouvre grand la porte du frigo, prépare tes sacs de 
+    congélation, et fais un dab pour dire adieu au gaspi.</p>
+    <p>Mais attention cela ne se fait pas n’importe comment, pour être sûr 
+    que ta nourriture est safe !</p>
+
+    <p>Pour en savoir plus, 
+        <a href="https://www.albal.fr/fr/les-10-regles-dor-pour-bien-congeler-et-decongeler-ses-aliments-612.html">clique ici</a> !</p>
     
     </Contenaire>
 </section>
