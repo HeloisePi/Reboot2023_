b5e5435b2c9bffd244b99e0977b7919eb4071431
evemarielouis <eve.tissot@mmibordeaux.com>
2023-11-16 16:17:20 +0200

    qui sommes-nous

--- src/routes/component/TitleH2.svelte
+++ src/routes/component/TitleH2.svelte
@@ -8,9 +8,10 @@
 <style>
 h2{
     border-top: 2px solid rgb(0, 0, 0);
+    margin-top: 3rem;
     color: #21864F;
-    padding-top: 1rem;
+    padding-top: 2rem;
     text-transform: uppercase;
-    margin-bottom: 5rem;
+    margin-bottom: 3rem;
 }
 </style>
